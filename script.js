84054a195bfdd2e32972e2131d34f5b9ae579faa
ananichoumchoum <la.annesmith@gmail.com>
2024-05-14 13:23:51 -0600

    changed value keys

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ const poems = [
     poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
     image: "bee.jpeg",
   },  
+  {
+  author: "Anne",
+  poem: "Roses are blue \n Violets are red \n Jerry Seinfeld is considering \n Making Bee Movie 2",
+  image: "roses_are_red.jpeg",
+},  
 ];
 
 let displayPoems = (array) => {
